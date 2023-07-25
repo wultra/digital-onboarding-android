5b8635e2422926777e803e4d3ed0fdbb4a5e3e28
Jan Kobersky <kober32@gmail.com>
2023-07-25 13:24:24 +0200

    Activation improvements

--- library/src/main/java/com/wultra/android/digitalonboarding/ActivationService.kt
+++ library/src/main/java/com/wultra/android/digitalonboarding/ActivationService.kt
@@ -19,6 +19,7 @@
 package com.wultra.android.digitalonboarding
 
 import android.content.Context
+import android.os.Build
 import com.wultra.android.digitalonboarding.networking.CustomerOnboardingApi
 import com.wultra.android.digitalonboarding.networking.GetStatusResponse
 import com.wultra.android.digitalonboarding.networking.OTPDetailResponse
@@ -27,8 +28,11 @@ import com.wultra.android.digitalonboarding.networking.StartOnboardingResponse
 import com.wultra.android.powerauth.networking.IApiCallResponseListener
 import com.wultra.android.powerauth.networking.data.StatusResponse
 import com.wultra.android.powerauth.networking.error.ApiError
+import io.getlime.security.powerauth.networking.exceptions.FailedApiException
+import io.getlime.security.powerauth.networking.response.CreateActivationResult
 import io.getlime.security.powerauth.sdk.PowerAuthSDK
 import okhttp3.OkHttpClient
+import java.lang.Exception
 
 /**
  * ActivationService
@@ -37,15 +41,9 @@ class ActivationService(
     identityServerUrl: String,
     appContext: Context,
     okHttpClient: OkHttpClient,
-    powerAuthSDK: PowerAuthSDK,
+    private val powerAuthSDK: PowerAuthSDK,
     canRestoreSession: Boolean = true
 ) {
-
-    init {
-        if (!canRestoreSession) {
-            setProcessId(null)
-        }
-    }
 
     /** PUBLIC PROPERTIES & CLASSES */
 
@@ -76,18 +74,25 @@ class ActivationService(
     private val api = CustomerOnboardingApi(identityServerUrl, okHttpClient, powerAuthSDK, appContext)
     private val storage = Storage(appContext, "wdo-prefs-encrypted")
     private val keychainKey = "wdopid_${powerAuthSDK.configuration.instanceId}"
+    private var processId: String?
+        get() = storage.getValue(keychainKey)
+        set(value) = storage.setValue(keychainKey, value)
+
+    init {
+        if (!canRestoreSession) {
+            processId = null
+        }
+    }
 
     /** PUBLIC API */
 
-    fun hasActiveProcess() = getProcessId() != null
+    fun hasActiveProcess() = processId != null
 
     fun status(callback: (Result<Status>) -> Unit) {
 
-        val processId = getProcessId()
-        if (processId == null) {
-            // TODO: error
-            return
-        }
+        val processId = guardProcessId(callback) ?: return
+
+        if (!verifyCanStartProcess(callback)) return
 
         api.getStatus(
             processId,
@@ -105,16 +110,19 @@ class ActivationService(
 
     fun <T> start(credentials: T, callback: (Result<Unit>) -> Unit) {
 
-        if (getProcessId() != null) {
-            // TODO: error
+        if (processId != null) {
+            // TODO: better exception
+            callback(Result.failure(Exception("TODO: better exception")))
             return
         }
+
+        if (!verifyCanStartProcess(callback)) return
 
         api.start(
             credentials,
             object : IApiCallResponseListener<StartOnboardingResponse> {
                 override fun onSuccess(result: StartOnboardingResponse) {
-                    setProcessId(result.responseObject.processId)
+                    processId = result.responseObject.processId
                     callback(Result.success(Unit))
                 }
 
@@ -127,23 +135,21 @@ class ActivationService(
 
     fun cancel(forceCancel: Boolean = true, callback: (Result<Unit>) -> Unit) {
 
-        val processId = getProcessId()
-        if (processId == null) {
-            // TODO: error
-            return
-        }
+        val processId = guardProcessId(callback) ?: return
+
+        if (!verifyCanStartProcess(callback)) return
 
         api.cancel(
             processId,
             object : IApiCallResponseListener<StatusResponse> {
                 override fun onSuccess(result: StatusResponse) {
-                    setProcessId(null)
+                    this@ActivationService.processId = null
                     callback(Result.success(Unit))
                 }
 
                 override fun onFailure(error: ApiError) {
                     if (forceCancel) {
-                        setProcessId(null)
+                        this@ActivationService.processId = null
                         callback(Result.success(Unit))
                     } else {
                         callback(Result.failure(error.toException()))
@@ -154,16 +160,12 @@ class ActivationService(
     }
 
     fun clear() {
-        setProcessId(null)
+        processId = null
     }
 
     fun getOtp(callback: (Result<String>) -> Unit) {
 
-        val processId = getProcessId()
-        if (processId == null) {
-            // TODO: error
-            return
-        }
+        val processId = guardProcessId(callback) ?: return
 
         api.getOtp(
             processId,
@@ -181,11 +183,9 @@ class ActivationService(
 
     fun resendOtp(callback: (Result<Unit>) -> Unit) {
 
-        val processId = getProcessId()
-        if (processId == null) {
-            // TODO: error
-            return
-        }
+        val processId = guardProcessId(callback) ?: return
+
+        if (!verifyCanStartProcess(callback)) return
 
         api.resendOtp(
             processId,
@@ -201,7 +201,56 @@ class ActivationService(
         )
     }
 
-    private fun getProcessId() = storage.getValue(keychainKey)
+    fun activate(
+        otp: String,
+        activationName: String = Build.MODEL,
+        callback: (Result<CreateActivationResult>) -> Unit
+    ) {
+        val processId = guardProcessId(callback) ?: return
 
-    private fun setProcessId(processId: String?) = storage.setValue(keychainKey, processId)
+        if (!verifyCanStartProcess(callback)) return
+
+        val data = ActivationDataWithOTP(processId, otp)
+
+        powerAuthSDK.createActivation(data, activationName) { result ->
+            result.onSuccess {
+                this.processId = null
+                callback(Result.success(it))
+            }.onFailure {
+                // when no longer possible to retry activation
+                // reset the processID, because we cannot recover
+                if ((it as? FailedApiException)?.allowOnboardingOtpRetry() == false) {
+                    this.processId = null
+                }
+                callback(Result.failure(it))
+            }
+        }
+    }
+
+    private fun <T>guardProcessId(callback: (Result<T>) -> Unit): String? {
+
+        val processId = this.processId
+        if (processId == null) {
+            // TODO: better exception
+            callback(Result.failure(Exception("TODO: better exception")))
+            return null
+        }
+        return processId
+    }
+
+    private fun <T>verifyCanStartProcess(callback: (Result<T>) -> Unit): Boolean {
+
+        if (!powerAuthSDK.canStartActivation()) {
+            processId = null
+            // TODO: better exception
+            callback(Result.failure(Exception("TODO: better exception")))
+            return false
+        }
+        return true
+    }
+}
+
+private data class ActivationDataWithOTP(val processId: String, val otp: String): ActivationData {
+    override fun processId() = processId
+    override fun asAttributes() = mapOf(Pair("processId", processId), Pair("otpCode", otp), Pair("credentialsType", "ONBOARDING"))
 }
---
+++ library/src/main/java/com/wultra/android/digitalonboarding/PowerAuthExtensions.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2023, Wultra s.r.o. (www.wultra.com).
+ *
+ * All rights reserved. This source code can be used only for purposes specified
+ * by the given license contract signed by the rightful deputy of Wultra s.r.o.
+ * This source code can be used only by the owner of the license.
+ *
+ * Any disputes arising in respect of this agreement (license) shall be brought
+ * before the Municipal Court of Prague.
+ */
+
+package com.wultra.android.digitalonboarding
+
+import io.getlime.security.powerauth.core.ActivationStatus
+import io.getlime.security.powerauth.networking.exceptions.FailedApiException
+import io.getlime.security.powerauth.networking.response.CreateActivationResult
+import io.getlime.security.powerauth.networking.response.ICreateActivationListener
+import io.getlime.security.powerauth.sdk.PowerAuthActivation
+import io.getlime.security.powerauth.sdk.PowerAuthSDK
+
+/** Activation data used in PowerAuth activation process (createActivation method). */
+interface ActivationData {
+    /** Process ID retrieved from `start` call. */
+    fun processId(): String
+    /** Attributes needed for the PowerAuth activation. */
+    fun asAttributes(): Map<String, String>
+}
+
+fun PowerAuthSDK.createActivation(
+    data: ActivationData,
+    activationName: String,
+    callback: (Result<CreateActivationResult>) -> Unit
+) {
+    val activation = PowerAuthActivation.Builder.customActivation(data.asAttributes(), activationName).build()
+    createActivation(
+        activation,
+        object : ICreateActivationListener {
+            override fun onActivationCreateSucceed(result: CreateActivationResult) {
+                callback(Result.success(result))
+            }
+
+            override fun onActivationCreateFailed(t: Throwable) {
+                callback(Result.failure(t))
+            }
+        }
+    )
+}
+
+fun FailedApiException.onboardingOtpRemainingAttempts(): Int? = responseJson?.get("remainingAttempts")?.asInt
+fun FailedApiException.allowOnboardingOtpRetry() = onboardingOtpRemainingAttempts()?.let { it > 0 }
+
+fun ActivationStatus.activationFlags() = (customObject["activationFlags"] as? List<*>)?.filterIsInstance<String>() ?: emptyList()
+fun ActivationStatus.verificationPending() = activationFlags().contains("VERIFICATION_PENDING")
+fun ActivationStatus.verificationInProgress() = activationFlags().contains("VERIFICATION_IN_PROGRESS")
+fun ActivationStatus.needVerification() = verificationPending() || verificationInProgress()
